ccb0b8f08bfc372194eefc5e82381ad318f2c9e6
Alok <boyeeaalok@gmail.com>
2026-02-19 16:40:19 -0600

    Add initial implementation of GroceryManagementSystem class

---
+++ GroceryManagementSystem.java
@@ -0,0 +1,10 @@
+
+public class GroceryManagementSystem {
+
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+    
+    }
